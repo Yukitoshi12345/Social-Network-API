c3d857c8eb9af217b36f201eb5a8bb5316ab28b4
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-30 02:00:02 +1100

    completed GET, POST, PUT, DELETE for /api/users

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,7 @@ module.exports = {
   async getSingleUser(req, res) {
     try {
       const user = await User.findOne({ _id: req.params.userId })
-        .populate('thougts')
+        .populate('thoughts')
         .populate('friends');
 
       if (!user) {
@@ -21,6 +21,48 @@ module.exports = {
       }
 
       res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true },
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user found with this id!' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.courseId });
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user found with that id!' });
+      }
+
+      await User.deleteMany({ _id: { $in: user.thoughts } });
+      res.json({ message: 'User and thoughts deleted!' });
     } catch (err) {
       res.status(500).json(err);
     }
