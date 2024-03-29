1ada532ec7c1ad87b497ae5673e29bd5ca333fea
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-29 23:06:05 +1100

    completed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,10 @@
+// Import the `connect` function and `connection` object from the 'mongoose' package
 const { connect, connection } = require('mongoose');
 
+// Establish a connection to the MongoDB database using Mongoose
+// The connection URL specifies the use of the local MongoDB instance and targets the 'developersApplications' database
 connect('mongodb://127.0.0.1:27017/developersApplications');
 
+// Export the `connection` object so it can be used elsewhere in the application
+// This object represents the default connection to the MongoDB database and can be used to listen to events, check the connection status, etc.
 module.exports = connection;
