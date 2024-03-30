8ed2c630b90644ab350cb9380b3e7ecc89496603
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-30 18:33:59 +1100

    completed PUT request for thoughtController.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -44,4 +44,24 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+
+  async updateThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $set: req.body },
+        { runValidators: true, new: true },
+      );
+
+      if (!thought) {
+        return res
+          .status(404)
+          .json({ message: 'No thought found with this id!' });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
