a49d13c57995d83454d8ed9ba8df0bd98c87e32c
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-03-31 14:50:53 +1100

    completing seeds but not fully completed

---
+++ utils/data.js
@@ -0,0 +1,31 @@
+const user = [
+  {
+    username: 'a',
+    email: 'a@gmail.com',
+  },
+  {
+    username: 'b',
+    email: 'b@gmail.com',
+  },
+  {
+    username: 'c',
+    email: 'c@gmail.com',
+  },
+];
+
+const thought = [
+  {
+    thoughtText: 'Yesterday is the new today',
+    username: 'a',
+  },
+  {
+    thoughtText: 'Tomorrow is the future of today',
+    username: 'b',
+  },
+  {
+    thoughtText: 'Sleep is for the weak',
+    username: 'c',
+  },
+];
+
+module.exports = { user, thought };
