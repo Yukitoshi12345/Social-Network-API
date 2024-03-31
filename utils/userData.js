12b509e79a7d28ade7589d1fc1877e33d64ff8b0
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-04-01 02:06:56 +1100

    completed seeds.js

--- utils/userData.js
+++ utils/userData.js
@@ -1,15 +1,53 @@
 const users = [
+  // User who loves to travel and explore new cultures
   {
-    username: 'acsjckd1231233',
-    email: 'a@gmail.com',
+    username: 'travel_enthusiast',
+    email: 'explore@travelworld.com',
   },
+  // User who enjoys coffee and often discusses different brews
   {
-    username: 'brsfjjmf123123',
-    email: 'b@gmail.com',
+    username: 'caffeine_queen',
+    email: 'morningbrew@coffeelovers.com',
   },
+  // User dedicated to lifelong learning and sharing knowledge
   {
-    username: 'cawdjdmadxm23',
-    email: 'c@gmail.com',
+    username: 'lifelong_learner',
+    email: 'curiosity@knowledgehub.com',
+  },
+  // Avid reader and literary critic with a penchant for classics
+  {
+    username: 'bookworm_1984',
+    email: 'pagesandplots@readersparadise.com',
+  },
+  // Amateur astronomer who loves sharing their telescope findings
+  {
+    username: 'astro_lover',
+    email: 'starrynight@cosmosfan.com',
+  },
+  // Programming enthusiast who enjoys discussing tech and coding
+  {
+    username: 'code_crusher',
+    email: 'loopsandfunctions@devtalk.com',
+  },
+  // Environmental activist focused on sustainability and green living
+  {
+    username: 'eco_warrior',
+    email: 'greenearth@ecosavers.com',
+  },
+  // Aspiring chef who shares their culinary journey and recipes
+  {
+    username: 'chef_in_the_making',
+    email: 'flavorsandspices@culinaryarts.com',
+  },
+  // Musician and composer who shares original tunes and music tips
+  {
+    username: 'melody_maestro',
+    email: 'tunesandkeys@musiclife.com',
+  },
+  // A user who appreciates and contributes ideas on innovation, fostering discussions on how it reshapes our understanding of what's possible
+  {
+    username: 'visionary_thinker',
+    email: 'ideas@futurehorizons.com',
   },
 ];
 
